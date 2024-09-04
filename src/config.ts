76e373d7532528f81d18e8d7b7341a456b135f24
Ricardo Casares <ricardocasares@gmail.com>
2024-09-04 05:31:12 +0200

    feat: use issues

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ import z from "zod";
 const config = z.object({
   TITLE: z.string(),
   DESCRIPTION: z.string(),
+  BASE: z.string(),
+  OWNER: z.string(),
   GITHUB_URL: z.string().url(),
   GITHUB_TOKEN: z.string(),
   GITHUB_REPOSITORY: z.string(),
